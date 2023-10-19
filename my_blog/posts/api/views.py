8185d968d28050957affdb2536c1692519498ed0
Sebastian Castro <scastro@qvision.com.co>
2023-10-19 01:06:49 -0500

    Serializadores,ViewSet,ModelViewSet

--- my_blog/posts/api/views.py
+++ my_blog/posts/api/views.py
@@ -1,10 +1,27 @@
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.views import APIView 
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
-from posts.models import Post
+from posts.models import Post 
+from posts.api.serializers import PostSerializer
 
-class PostApiView(APIView):
-    def get(self, request):
-       # posts = Post.objects.all()
-       posts = [post.title for post in Post.objects.all()]
-       return Response(status=status.HTTP_200_OK, data=posts)
+class PostModelViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+""" class PostViewSet(ViewSet):
+    def list(self, request):
+       posts = PostSerializer(Post.objects.all(),many=True)
+       return Response(status=status.HTTP_200_OK, data=posts.data) 
+   
+    def retrieve(self, request,pk: int):
+        post = PostSerializer(Post.objects.get(pk=pk))
+        return Response(status=status.HTTP_200_OK, data=post.data)
+   
+    def create(self, request):
+        posts = PostSerializer(data=request.POST)
+        posts.is_valid(raise_exception=True)
+        posts.save()
+        return Response(status=status.HTTP_200_OK, data=posts.data) """
+        
+  
--- my_blog/posts/models.py
+++ my_blog/posts/models.py
@@ -4,4 +4,4 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     order = models.IntegerField()
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
