1a40246bf9fa6ef27278854934e75c8e0f1fad9d
Sebastian Castro <scastro@qvision.com.co>
2023-10-21 22:55:52 -0500

    creamos la palicacion, el super usuario , configuramos django-rest-framework

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'users'
+    
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = 'users.User'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
